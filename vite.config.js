8487ce03420f8bd4b1a1f4baeccb64fe2583e136
Napatchouli <n.sampatpong@gmail.com>
2025-06-29 02:57:04 +0700

    Second

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Napatchouli/React-Warfarin-V2',
   plugins: [react(),tailwindcss()],
 })
